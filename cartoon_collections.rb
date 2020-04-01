4864d1d5b21b3604452bb38184e60394b2148739
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-01 02:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,14 +8,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |i|
     i.capitalize + "!"
   end
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|word| word.length > 4} 
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
 end
 
 def find_valid_calls(planeteer_calls)
